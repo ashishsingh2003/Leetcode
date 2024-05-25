8a15c53023bac5fc83e44e677d291406f1b9947d
ashishsinghrana39@gmail.com <106158861+ashishsingh2003@users.noreply.github.com>
2024-05-25 22:06:36 +0530

    committed

--- day1.cpp
+++ day1.cpp
@@ -1,9 +1,6 @@
-// Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
-
+// Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+// Return all such possible sentences in any order.
 // Note that the same word in the dictionary may be reused multiple times in the segmentation.
-
- 
-
 // Example 1:
 
 // Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
@@ -55,5 +52,40 @@ public:
         }
         
         return ans;
+    }
+};
+
+
+// -------------------GFG Question---------------------
+
+// You have n stacks of books. Each stack of books has some nonzero height arr[i] equal to the number of books on that stack ( considering all the books are identical and each book has a height of 1 unit ). In one move, you can select any number of consecutive stacks of books such that the height of each selected stack of books arr[i] <= k. Once such a sequence of stacks is chosen, You can collect any number of books from the chosen sequence of stacks.
+// What is the maximum number of books that you can collect this way?
+
+// Example 1
+
+// Input
+// 8 1
+// 3 2 2 3 1 1 1 3
+// Output
+// 3
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        // Your code here
+        long long maxi=0;
+        long long sum=0;
+        for(long long i=0;i<n;i++)
+        {
+            if(arr[i]<=k)
+            {
+                sum+=arr[i];
+                maxi=max(maxi,sum);
+            }
+            else{
+                sum=0;
+            }
+        }
+        return maxi;
     }
 };
