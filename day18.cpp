a86c2aca28c68ec8f040d20802db6555826b8209
ashishsinghrana39@gmail.com <106158861+ashishsingh2003@users.noreply.github.com>
2024-06-18 17:00:21 +0530

    committed

---
+++ day18.cpp
@@ -0,0 +1,39 @@
+// You have n jobs and m workers. You are given three arrays: difficulty, profit, and worker where:
+
+// difficulty[i] and profit[i] are the difficulty and the profit of the ith job, and
+// worker[j] is the ability of jth worker (i.e., the jth worker can only complete a job with difficulty at most worker[j]).
+// Every worker can be assigned at most one job, but one job can be completed multiple times.
+
+// For example, if three workers attempt the same job that pays $1, then the total profit will be $3. If a worker cannot complete any job, their profit is $0.
+// Return the maximum profit we can achieve after assigning the workers to the jobs.
+
+ 
+
+// Example 1:
+
+// Input: difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7]
+// Output: 100
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& d, vector<int>& p, vector<int>& w) {
+        int ans=0;
+        for(int i=0;i<w.size();i++)
+        {
+            int x=INT_MIN;
+            int ind=-1;
+            for(int j=0;j<d.size();j++)
+            {
+                if(w[i]>=d[j])
+                {
+                  x=max(p[j],x);
+                  if(x==p[j]){
+                  ind=j;
+                  }
+                }
+            }
+            if(ind!=-1)
+            ans+=x;
+        }
+        return ans;
+    }
+};
